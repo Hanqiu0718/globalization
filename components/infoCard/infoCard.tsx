f369003b125025ef39711aed48cfb50808d39564
mts755Dev <muhammadtalhasaleem.755@gmail.com>
2024-05-02 03:20:17 +0100

    page 2 started

--- components/infoCard/infoCard.tsx
+++ components/infoCard/infoCard.tsx
@@ -1,22 +1,68 @@
 'use client';
 
-import { Card, CardDescription } from '../ui/card';
+import { useForm } from 'react-hook-form';
+import { Card, CardContent, CardDescription } from '../ui/card';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { toast } from '../ui/use-toast';
 
 export function InfoCard() {
+  const FormSchema = z.object({
+    id: z.string().min(3, {
+      message: 'Id must be at least 3 characters.',
+    }),
+  });
 
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      id: '',
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    if (!data.id) return;
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
 
   return (
-    <form>
       <Card className="w-full border md:border-[2px] flex-col items-center justify-center mb-10">
         <CardDescription className="font-semibold text-xl border-b md:border-b-0 mt-3 text-[#212B36] md:mx-5 pb-3 md:pb-0">
-          LinkedIn Profile Review
+          Your Details
         </CardDescription>
-        <CardDescription className="mt-10 text-[#212B36] md:px-5 md:mx-5">
-          Upload your LinkedIn Profile to get it reviewed to best standard. You
-          will get all the information on every area that needs to be adjusted
-          at the end of the review.
-        </CardDescription>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-[#637381]">Mturk ID Number*</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="text-[#637381] border-0 md:border border-b"
+                        placeholder="Enter Mturk ID Number"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+          </CardContent>
       </Card>
-    </form>
   );
 }
